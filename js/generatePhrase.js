2b3e754ef50f0d193b53fe113a23da9bc6a86dbd
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-05 20:05:50 +0200

    add fade in effect

--- js/generatePhrase.js
+++ js/generatePhrase.js
@@ -1,4 +1,12 @@
 function generatePhrase() {
   let phrase = faker.hacker.phrase();
   document.getElementById('smart-phrase').innerHTML = phrase;
+
+  // RESTART ANIMATION
+  // - clone element
+  // - delete the original
+  // - insert the clone
+  let element = document.getElementById('smart-phrase');
+  let newElement = element.cloneNode(true);
+  element.parentNode.replaceChild(newElement, element);
 }
